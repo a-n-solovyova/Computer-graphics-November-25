d18f25e83267877306bc09bbd30597025bebea34
a-n-solovyova <73398713+a-n-solovyova@users.noreply.github.com>
2020-11-25 20:22:53 +0400

    Add files via upload

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -60,11 +61,22 @@
             this.openFileDialog1.InitialDirectory = "C:\\\\";
             this.openFileDialog1.RestoreDirectory = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Filter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(689, 525);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -78,6 +90,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -23,25 +23,96 @@ namespace WindowsFormsApp1
         {
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                Graphics myGraphics = panel1.CreateGraphics(); //creates a canvas to draw on
-                
                 myBitmap = new Bitmap(openFileDialog1.FileName);
-
-                RectangleF sourceRectangle =
-                    new RectangleF(0, 0, myBitmap.Width, myBitmap.Height);
-
-                RectangleF destinationRectangle =
-                    new RectangleF(0, 0, panel1.Width, panel1.Height);
-                                
-                myGraphics.DrawImage(myBitmap, destinationRectangle,
-                    sourceRectangle, GraphicsUnit.Pixel);
-
-                myGraphics.Dispose(); // clear the memory allocated for the canvas
+                DrawBitmap(myBitmap, panel1);               
 
             }
             
         }
 
+        private void DrawBitmap(Bitmap bmp, Control control)
+        {
+            Graphics myGraphics = control.CreateGraphics(); //creates a canvas to draw on
 
+            RectangleF sourceRectangle =
+                new RectangleF(0, 0, bmp.Width, bmp.Height);
+
+            RectangleF destinationRectangle =
+                new RectangleF(0, 0, control.Width, control.Height);
+
+            myGraphics.DrawImage(bmp, destinationRectangle,
+                sourceRectangle, GraphicsUnit.Pixel);
+
+            myGraphics.Dispose(); // clear the memory allocated for the canvas
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap newBitmap = new Bitmap(myBitmap);
+
+            //---Filtering myBitmap into newBitmap---
+
+            float[][] filterKernelMatrix =
+            {
+                new float[] { 0.125f, 0.0625f, 0.125f },
+                new float[] { 0.125f, 0.25f, 0.125f },
+                new float[] { 0.125f, 0.0625f, 0.125f }
+            };
+
+            for (int i = 1; i < newBitmap.Width-1; i++)
+            {
+                for (int j = 1; j < newBitmap.Height-1; j++)
+                {
+                    float red =
+                        myBitmap.GetPixel(i - 1, j - 1).R * filterKernelMatrix[0][0] +
+                        myBitmap.GetPixel(i, j - 1).R * filterKernelMatrix[0][1] +
+                        myBitmap.GetPixel(i + 1, j - 1).R * filterKernelMatrix[0][2] +
+                        myBitmap.GetPixel(i - 1, j).R * filterKernelMatrix[1][0] +
+                        myBitmap.GetPixel(i, j).R * filterKernelMatrix[1][1] +
+                        myBitmap.GetPixel(i + 1, j).R * filterKernelMatrix[1][2] +
+                        myBitmap.GetPixel(i - 1, j + 1).R * filterKernelMatrix[2][0] +
+                        myBitmap.GetPixel(i, j + 1).R * filterKernelMatrix[2][1] +
+                        myBitmap.GetPixel(i + 1, j + 1).R * filterKernelMatrix[2][2];
+
+                    red = red < 0 ? 0 : red;   // if red < 0 then red = 0 else red = red
+                    red = red > 255 ? 255 : red; // if red > 255 then red = 255 else red = red
+                                    
+                    // < Condition > ? < Action If True > : < Action If False >
+
+                    float green =
+                        myBitmap.GetPixel(i - 1, j - 1).G * filterKernelMatrix[0][0] +
+                        myBitmap.GetPixel(i, j - 1).G * filterKernelMatrix[0][1] +
+                        myBitmap.GetPixel(i + 1, j - 1).G * filterKernelMatrix[0][2] +
+                        myBitmap.GetPixel(i - 1, j).G * filterKernelMatrix[1][0] +
+                        myBitmap.GetPixel(i, j).G * filterKernelMatrix[1][1] +
+                        myBitmap.GetPixel(i + 1, j).G * filterKernelMatrix[1][2] +
+                        myBitmap.GetPixel(i - 1, j + 1).G * filterKernelMatrix[2][0] +
+                        myBitmap.GetPixel(i, j + 1).G * filterKernelMatrix[2][1] +
+                        myBitmap.GetPixel(i + 1, j + 1).G * filterKernelMatrix[2][2];
+
+                    green = green < 0 ? 0 : green;
+                    green = green > 255 ? 255 : green;
+
+                    float blue =
+                        myBitmap.GetPixel(i - 1, j - 1).B * filterKernelMatrix[0][0] +
+                        myBitmap.GetPixel(i, j - 1).B * filterKernelMatrix[0][1] +
+                        myBitmap.GetPixel(i + 1, j - 1).B * filterKernelMatrix[0][2] +
+                        myBitmap.GetPixel(i - 1, j).B * filterKernelMatrix[1][0] +
+                        myBitmap.GetPixel(i, j).B * filterKernelMatrix[1][1] +
+                        myBitmap.GetPixel(i + 1, j).B * filterKernelMatrix[1][2] +
+                        myBitmap.GetPixel(i - 1, j + 1).B * filterKernelMatrix[2][0] +
+                        myBitmap.GetPixel(i, j + 1).B * filterKernelMatrix[2][1] +
+                        myBitmap.GetPixel(i + 1, j + 1).B * filterKernelMatrix[2][2];
+
+                    blue = blue < 0 ? 0 : blue;
+                    blue = blue > 255 ? 255 : blue;
+
+                    newBitmap.SetPixel(i, j, Color.FromArgb((int)red, (int)green, (int)blue));
+                }
+            }
+
+            DrawBitmap(newBitmap, panel1);
+        }
     }
 }
