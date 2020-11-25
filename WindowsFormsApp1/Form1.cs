0e78fad2ade8b503a68966c03096e88e8824c0ba
a-n-solovyova <73398713+a-n-solovyova@users.noreply.github.com>
2020-11-25 18:51:04 +0400

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging; // namespace that contains PixelFormat
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap myBitmap;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Graphics myGraphics = panel1.CreateGraphics(); //creates a canvas to draw on
+                
+                myBitmap = new Bitmap(openFileDialog1.FileName);
+
+                RectangleF sourceRectangle =
+                    new RectangleF(0, 0, myBitmap.Width, myBitmap.Height);
+
+                RectangleF destinationRectangle =
+                    new RectangleF(0, 0, panel1.Width, panel1.Height);
+                                
+                myGraphics.DrawImage(myBitmap, destinationRectangle,
+                    sourceRectangle, GraphicsUnit.Pixel);
+
+                myGraphics.Dispose(); // clear the memory allocated for the canvas
+
+            }
+            
+        }
+
+
+    }
+}
